6d9a564c456c817b38446459dc5237789d91e743
itTeruya <timur.ivanov@outlook.com>
2021-06-26 15:57:20 +0300

    110

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     add_data_2 = test_data.get_add_sphere_2()
     add_data_3 = test_data.get_add_sphere_3()
 
-    temp = add_data[20:80].copy()
-    add_data[20:80] = add_data_2[20:80]
-    add_data_2[20:80] = temp
+    temp = add_data[50:80].copy()
+    add_data[50:80] = add_data_2[50:80]
+    add_data_2[50:80] = temp
 
     net = network(num_fluctuations=400)
 
