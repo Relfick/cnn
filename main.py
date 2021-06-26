fd7277e7997a7a145b467ee548b95faf4958549e
Relfick <40392607+Relfick@users.noreply.github.com>
2021-06-26 18:18:19 +0300

    b added

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     add_data_2 = test_data.get_add_sphere_2()
     add_data_3 = test_data.get_add_sphere_3()
 
-    temp = add_data[44:80]
-    add_data[44:80] = add_data_2[44:80]
-    add_data_2[44:80] = temp
-    a = 5
-    print(temp / a)
+    temp = add_data[50:80]
+    add_data[50:80] = add_data_2[50:80]
+    add_data_2[50:80] = temp
+    b = 5
+    print(temp / b)
 
     net = network(num_fluctuations=400)
 
