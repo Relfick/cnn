1140182ddf3a40bc657c886d58670a83f92b4152
relfick <relfick@yandex.ru>
2021-06-26 18:10:15 +0300

    copy

--- main.py
+++ main.py
@@ -10,10 +10,8 @@ if __name__ == "__main__":
     add_data_3 = test_data.get_add_sphere_3()
 
     temp = add_data[44:80].copy()
-    b = 11
     add_data[44:80] = add_data_2[44:80]
     add_data_2[44:80] = temp
-    print(add_data * b)
 
     net = network(num_fluctuations=400)
 
