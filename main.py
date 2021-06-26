f32cb3b5728d3e4ec558d7804ba18b714d07b8a4
relfick <relfick@yandex.ru>
2021-06-26 18:13:58 +0300

    a added b added

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ if __name__ == "__main__":
     add_data_2 = test_data.get_add_sphere_2()
     add_data_3 = test_data.get_add_sphere_3()
 
-    temp = add_data[50:80]
+    temp = add_data[50:80].copy()
     add_data[50:80] = add_data_2[50:80]
     add_data_2[50:80] = temp
+    a = 5
     b = 5
+    print(temp / a)
     print(temp / b)
 
     net = network(num_fluctuations=400)
